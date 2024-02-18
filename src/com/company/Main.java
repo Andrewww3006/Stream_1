1cc7f94681f6650376163115817a53f3ecb98ae2
Andrewww3006 <andrew_3006@mail.ru>
2024-02-18 23:15:24 +0300

    exception commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
         ArrayList<String> strings = new ArrayList<>(Arrays.asList("a1","a2","a3","a8"));
         System.out.println("кол-во вхождений а1 в коллекцию: " + strings.stream().filter(x-> x.equals("a1")).count());
         Optional<String> first = strings.stream().findFirst();
@@ -16,6 +16,16 @@ public class Main {
         long count = strings.stream().count();
         Optional<String> last2 = strings.stream().skip(count-1).findFirst();
         System.out.println("последний элемент коллекции: " + last2.orElse("empty"));
+        try {
+
+            if(strings.stream().anyMatch(x->x=="a5"))
+            {strings.stream().filter(x -> x == "a3").forEach(System.out::println);}
+            else throw new MyException();
+        }
+
+        catch (MyException myException) {
+            myException.printStackTrace();
+        }
 
 
     }
