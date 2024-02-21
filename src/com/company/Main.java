753ef7316815df4ad4a223238fd6bf069bb540c8
Andrewww3006 <andrew_3006@mail.ru>
2024-02-21 22:42:26 +0300

    finish commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) throws MyException {
-        ArrayList<String> strings = new ArrayList<>(Arrays.asList("a1","a2","a3","a8"));
+        ArrayList<String> strings = new ArrayList<>(Arrays.asList("a1","a2","a3","a1", "a1"));
         System.out.println("кол-во вхождений а1 в коллекцию: " + strings.stream().filter(x-> x.equals("a1")).count());
         Optional<String> first = strings.stream().findFirst();
         System.out.println("первый элемент коллекции: " + first.orElse("0"));
@@ -18,7 +18,7 @@ public class Main {
         System.out.println("последний элемент коллекции: " + last2.orElse("empty"));
         try {
 
-            if(strings.stream().anyMatch(x->x=="a5"))
+            if(strings.stream().anyMatch(x->x=="a3"))
             {strings.stream().filter(x -> x == "a3").forEach(System.out::println);}
             else throw new MyException();
         }
@@ -26,7 +26,16 @@ public class Main {
         catch (MyException myException) {
             myException.printStackTrace();
         }
-
+        Optional<String>third =strings.stream().skip(2).findFirst();
+        System.out.println("третий элемент:" + third.get());
+        System.out.print("2 элемента начиная со второго: ");
+        strings.stream().skip(1).limit(2).forEach(System.out::print);
+        System.out.println();
+        System.out.print("выводит шаблон a1: ");
+        strings.stream().forEach(s -> {
+            if(s=="a1")
+                System.out.printf(s +  ", ");
+        });
 
     }
 }
